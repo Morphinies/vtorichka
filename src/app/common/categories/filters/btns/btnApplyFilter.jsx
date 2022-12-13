9ad314356209ddb3b6a5001b97b5b166163f2134
Cyber Cat <ilya-novyy@mail.ru>
2022-12-13 23:39:18 +0300

    waveEffect

--- src/app/common/categories/filters/btns/btnApplyFilter.jsx
+++ src/app/common/categories/filters/btns/btnApplyFilter.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
 import s from "../../../categories/categories.module.css";
 
 const BtnApplyFilter = ({
@@ -14,18 +15,43 @@ const BtnApplyFilter = ({
     );
   };
 
+  let timeOut = useRef();
+  const [waveStyle, setWaveStyle] = useState("");
+
+  console.log(waveStyle);
+
+  const waveOn = () => {
+    // волновой эффект
+    if (!!waveStyle) {
+      setWaveStyle("");
+      setTimeout(() => {
+        setWaveStyle(s.wave);
+      }, 100);
+    } else if (!waveStyle) {
+      setWaveStyle(s.wave);
+    }
+
+    // таймер выполнения волнового эффекта
+    timeOut.current && clearTimeout(timeOut.current);
+    timeOut.current = setTimeout(() => {
+      setWaveStyle("");
+      timeOut.current = undefined;
+    }, 2000);
+  };
+
   return (
     <button
       type="button"
       id={s.catBtnApply}
       className={
         s.categoriesBtn +
-        " " +
-        (equalObjects(choosedFilters, appliedFilters) && "disabled")
+        (equalObjects(choosedFilters, appliedFilters) ? " disabled " : " ") +
+        waveStyle
       }
       onClick={() => {
         if (equalObjects(choosedFilters, appliedFilters)) return;
         applyFilters(choosedFilters);
+        waveOn();
       }}
     >
       <p className={s.categoriesBtnText}>{name}</p>
