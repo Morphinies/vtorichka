b11d3fa29e3a19bd0d49a53f04ee322d7cc4a131
Morphinies <90818986+Morphinies@users.noreply.github.com>
2023-07-27 14:40:38 +0300

    clear root

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Header from "./app/common/header/header";
 import Footer from "./app/common/footer/footer";
